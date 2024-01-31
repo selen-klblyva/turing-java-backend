01b81ec4ced83d7f5a47eadd036504fd51cda003
Selen <kalbaliyevaselen@gmail.com>
2024-01-31 16:14:21 +0400

    Sum of ALL numbers

---
+++ src/main/java/az/edu/turing/SumOfNumbersApp.java
@@ -0,0 +1,20 @@
+package az.edu.turing;
+
+import java.util.Scanner;
+public class SumOfNumbersApp {
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        double[] array=new double[n];
+        int i;
+        for(i=0;i<n;i++){
+            array[i]=scanner.nextDouble();
+        }
+        double sum=0;
+        for(i=0;i<n;i++){
+            sum+=array[i];
+        }
+        System.out.printf("%.lf",sum);
+
+    }
+}
